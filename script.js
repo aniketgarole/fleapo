44c98ce361e70310df81dbabcbf15a751ca50fdf
Aniket Garole <aniketgarole11@gmail.com>
2024-05-13 00:27:48 +0530

    questions section completed

--- script.js
+++ script.js
@@ -6,7 +6,30 @@ navbarTabs.forEach(element => {
     element.addEventListener("click", function(e){
         e.preventDefault()
         navbarTabs.forEach(tab => tab.classList.remove('active'));
-        console.log(this)
         this.classList.add('active');
     })
+});
+
+
+let answers = document.querySelectorAll(".view-answer") 
+
+answers.forEach(item => {
+    const question = item.querySelector('.question');
+    const answer = item.querySelector('.answer');
+
+    
+
+    item.addEventListener('click', function() {
+        answers.forEach(el => {
+            const el_ans = el.querySelector('.answer')
+            const el_que = el.querySelector('.question')
+            if (this !== el) {
+
+                el_ans.classList.remove('active')
+                el_que.classList.remove('active')
+            }
+        })
+        answer.classList.toggle('active');
+        question.classList.toggle('active')
+    });
 });
